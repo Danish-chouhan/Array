3deb27d0b2200999b7398cbb7f129374bf9012cf
Danish-chouhan <danishchouhan41@gmail.com>
2023-05-11 00:49:16 +0530

    Array

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// forEach
+
+// const number = [1,2,3,4]
+
+// let sum = 0;
+
+// number.forEach((item,ik,arr)=>{
+//     let summing = sum += item
+//     console.log(`array have a index ${ik} and the num on this index is ${item} and the total sum of array is ${summing} and this is comming from this array ${arr}`)
+// }) ;
+
+// ----------------------------
+
+// const latters = ["a", "b", "b", "c", "a"]
+
+// let obj = {};
+
+// latters.forEach((item)=>{
+//     if(obj[item]){
+//         obj[item]++
+//     }else{
+//         obj[item] = 1
+//     }
+// })
+// console.log(obj)
+
+
